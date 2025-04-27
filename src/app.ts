bcf120247aa08f220d4c9f9d4b8091156415c2da
ralapple <ralapple@thelyonfamily.net>
2025-04-26 20:59:12 -0500

    Added middlewear

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,12 @@ import express, { Express } from 'express';
 import { IDatabaseContext } from './db/IDatabaseContext';
 import { ILogger } from './util/ILogger';
 import { ConsoleLogger } from './util/ConsoleLogger';
+import rateLimit from 'express-rate-limit';
 
 /**
  * Interface to define app functionality.
  */
 interface IApp {
-    initializeServices(): void;
     start(): void;
 };
 
@@ -20,12 +20,36 @@ class App implements IApp {
         this.logger = new ConsoleLogger();
     }
 
-    public initializeServices(): void {
-        
+    private configureMiddlewear(): void {
+
+        // Limit the request sizes
+        this.server.use(express.json({limit: '1mb'}));
+
+        // Rate limiting
+        const limiter = rateLimit({
+            windowMs: 15 * 60 * 1000,
+            max: 100,
+            standardHeaders: true,
+            legacyHeaders: false,
+            message: 'Too many requests from this IP, please try again after 15 minutes'
+        });
+        this.server.use(limiter);
+
+        // Remove certain headers
+        this.server.disable('x-powered-by');
+
+        // Add logging to each request.
+    }
+
+    private initializeServices(): void {
+        this.logger.info("Services initialized.")
     }
 
     public start(): void {
         const port = 3000;
+        this.configureMiddlewear();
+        this.initializeServices();
+
         this.server.listen(port, () => {
             this.logger.info(`Express is listening at http://localhost:${port}`);
         });
