817b864450d6ec93152cb310ab0c8f88f9a9017c
ralapple <ralapple@thelyonfamily.net>
2025-05-08 18:53:49 -0500

    Fixed comment

--- src/util/decorator/AuthenticateDecorator.ts
+++ src/util/decorator/AuthenticateDecorator.ts
@@ -2,8 +2,8 @@ import { UseBefore } from 'routing-controllers';
 import { AuthMiddleware } from '../../middlewear/AuthMiddleware';
 
 /**
- * Custom decorator for applying logging middleware.
- * @returns Decorator function that applies LoggingMiddleware
+ * Custom decorator for applying authentication middleware.
+ * @returns Decorator function that applies AuthMiddleware.
  */
 export function Authenticated(): ClassDecorator & MethodDecorator {
     return UseBefore(AuthMiddleware) as ClassDecorator & MethodDecorator;
